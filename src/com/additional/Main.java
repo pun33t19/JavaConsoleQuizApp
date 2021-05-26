e38400c17406d5db8cf16f956545883e2350151d
Puneet <puneet22088@gmail.com>
2021-05-26 22:13:36 +0530

    Update Main.java

--- src/com/additional/Main.java
+++ src/com/additional/Main.java
@@ -16,42 +16,8 @@ import java.util.Scanner;
 //
 //     }
 // }
- class Csv {
-    public void timer(long num) throws InterruptedException {
-        int charsWritten = 0;
-        while (1 > 0) {
-            Thread.sleep(1000);
-            long elapsedTime = System.currentTimeMillis() - num;
-            elapsedTime = elapsedTime / 1000;
-
-            String seconds = Integer.toString((int) (elapsedTime % 60));
-            String minutes = Integer.toString((int) ((elapsedTime % 3600) / 60));
-            String hours = Integer.toString((int) (elapsedTime / 3600));
-
-            if (seconds.length() < 2) {
-                seconds = "0" + seconds;
-            }
-
-            if (minutes.length() < 2) {
-                minutes = "0" + minutes;
-            }
-
-            if (hours.length() < 2) {
-                hours = "0" + hours;
-            }
-
-            String writeThis = hours + ":" + minutes + ":" + seconds;
-
-            for (int i = 0; i < charsWritten; i++) {
-                System.out.print("\b");
-            }
-            System.out.print(writeThis);
-            charsWritten = writeThis.length();
-        }
-
-    }
-
-    public void toReadFile(String root, byte num) throws IOException, CsvValidationException, InterruptedException {
+ class Csv {  
+  public void toReadFile(String root, byte num) throws IOException, CsvValidationException, InterruptedException {
 
         FileReader reader = new FileReader(root);
 
